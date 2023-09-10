777884e84dd33b005bab33ae420a2c840efb4691
Mikhail Matveichuk <mihkailmatveichuk@gmail.com>
2023-09-10 20:27:38 +0300

    feat: add logger interface

--- src/logger/logger.service.ts
+++ src/logger/logger.service.ts
@@ -1,26 +1,27 @@
-import { Logger} from "tslog";
+import { Logger } from "tslog";
+import { ILogger } from "./logger.interface";
 
-export class LoggerService{
-    private logger: Logger;
+export class LoggerService implements ILogger {
+  public logger: Logger;
 
-    constructor(){
-        this.logger = new Logger({ 
-            displayInstanceName: false,
-            displayLoggerName: false,
-            displayFilePath: 'hidden',
-            displayFunctionName: false,
-        });
-    }
+  constructor() {
+    this.logger = new Logger({
+      displayInstanceName: false,
+      displayLoggerName: false,
+      displayFilePath: "hidden",
+      displayFunctionName: false,
+    });
+  }
 
-    log(...args: unknown[]){
-        this.logger.info(...args)
-    }
-    
-    error(...args: unknown[]){
-        this.logger.error(...args)
-    }
-    
-    warn(...args: unknown[]){
-        this.logger.warn(...args)
-    }
+  log(...args: unknown[]) {
+    this.logger.info(...args);
+  }
+
+  error(...args: unknown[]) {
+    this.logger.error(...args);
+  }
+
+  warn(...args: unknown[]) {
+    this.logger.warn(...args);
+  }
 }
