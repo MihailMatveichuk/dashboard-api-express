8e3c1641920f73a6614e1dba4f9d16d5c3607383
Mikhail Matveichuk <mihkailmatveichuk@gmail.com>
2023-09-13 19:52:59 +0300

    feat: add eslint and prettier configs

--- src/errors/exeption.filter.ts
+++ src/errors/exeption.filter.ts
@@ -1,29 +1,22 @@
-import { NextFunction, Request, Response } from "express";
-import { IExeptionFilter } from "./exeption.filter.interface";
-import { HttpError } from "./http.error";
-import { injectable, inject } from "inversify";
-import { ILogger } from "../logger/logger.interface";
-import { TYPES } from "../types";
-import "reflect-metadata";
+import { NextFunction, Request, Response } from 'express';
+import { IExeptionFilter } from './exeption.filter.interface';
+import { HttpError } from './http.error';
+import { injectable, inject } from 'inversify';
+import { ILogger } from '../logger/logger.interface';
+import { TYPES } from '../types';
+import 'reflect-metadata';
 
 @injectable()
 export class ExeptionFilter implements IExeptionFilter {
-  constructor(@inject(TYPES.ILogger) private logger: ILogger) {}
+	constructor(@inject(TYPES.ILogger) private logger: ILogger) {}
 
-  catch(
-    error: Error | HttpError,
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) {
-    if (error instanceof HttpError) {
-      this.logger.error(
-        `[${error.context}] Error: ${error.statusCode} ${error.message}`
-      );
-      res.status(error.statusCode).send({ err: error.message });
-    } else {
-      this.logger.error(`${error.message}`);
-      res.status(500).send({ err: error.message });
-    }
-  }
+	catch(error: Error | HttpError, req: Request, res: Response, next: NextFunction): void {
+		if (error instanceof HttpError) {
+			this.logger.error(`[${error.context}] Error: ${error.statusCode} ${error.message}`);
+			res.status(error.statusCode).send({ err: error.message });
+		} else {
+			this.logger.error(`${error.message}`);
+			res.status(500).send({ err: error.message });
+		}
+	}
 }
--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,30 @@
-import { Container, ContainerModule, interfaces } from "inversify";
-import { App } from "./app";
-import { ExeptionFilter } from "./errors/exeption.filter";
-import { LoggerService } from "./logger/logger.service";
-import { UserController } from "./users/users.controller";
-import { ILogger } from "./logger/logger.interface";
-import { TYPES } from "./types";
-import { IExeptionFilter } from "./errors/exeption.filter.interface";
-import { IUserController } from "./users/users.controller.interface";
+import { Container, ContainerModule, interfaces } from 'inversify';
+import { App } from './app';
+import { ExeptionFilter } from './errors/exeption.filter';
+import { LoggerService } from './logger/logger.service';
+import { UserController } from './users/users.controller';
+import { ILogger } from './logger/logger.interface';
+import { TYPES } from './types';
+import { IExeptionFilter } from './errors/exeption.filter.interface';
+import { IUserController } from './users/users.controller.interface';
 
+interface IBootstrap {
+	appContainer: Container;
+	app: App;
+}
 export const appBindings = new ContainerModule((bind: interfaces.Bind) => {
-  bind<ILogger>(TYPES.ILogger).to(LoggerService);
-  bind<IExeptionFilter>(TYPES.IExeptionFilter).to(ExeptionFilter);
-  bind<IUserController>(TYPES.IUserController).to(UserController);
-  bind<App>(TYPES.Application).to(App);
+	bind<ILogger>(TYPES.ILogger).to(LoggerService);
+	bind<IExeptionFilter>(TYPES.IExeptionFilter).to(ExeptionFilter);
+	bind<IUserController>(TYPES.IUserController).to(UserController);
+	bind<App>(TYPES.Application).to(App);
 });
 
-function bootstrap() {
-  const appContainer = new Container();
-  appContainer.load(appBindings);
-  const app = appContainer.get<App>(TYPES.Application);
-  app.init();
-  return { appContainer, app };
+function bootstrap(): IBootstrap {
+	const appContainer = new Container();
+	appContainer.load(appBindings);
+	const app = appContainer.get<App>(TYPES.Application);
+	app.init();
+	return { appContainer, app };
 }
 
 export const { app, appContainer } = bootstrap();
